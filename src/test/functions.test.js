71340b6236b171d3466b83c1f11a1fb77d307d10
ichala <alijandhd@gmail.com>
2022-05-03 12:03:12 +0100

    Add Delete test and Fix Linters

--- src/test/functions.test.js
+++ src/test/functions.test.js
@@ -1,18 +1,22 @@
-import { add,DeleteItem } from "../modules/functions";
-
+import { add, DeleteItem } from '../modules/functions.js';
 
 describe('Add and Delete Function', () => {
-    test('add item to array', () => { 
-        const localStorageMock = {
-            getItem: jest.fn(),
-            setItem: jest.fn(),
-            clear: jest.fn()
-          };
-          global.localStorage = localStorageMock;
-        let array= []
-        add(array, 'this is a test', false);
-        console.log(array);
-        expect(array).toEqual([{description: "this is a test", completed: false, index: 1}])
-        expect(global.localStorage.setItem).toBeCalledWith("tasks",JSON.stringify(array))
-     })
-})
+  const localStorageMock = {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    clear: jest.fn(),
+  };
+  global.localStorage = localStorageMock;
+  test('add item to array', () => {
+    const array = [];
+    add(array, 'this is a test', false);
+    expect(array).toEqual([{ description: 'this is a test', completed: false, index: 1 }]);
+    expect(global.localStorage.setItem).toBeCalledWith('tasks', JSON.stringify(array));
+  });
+  test('delete item from array', () => {
+    const array = [];
+    DeleteItem(array, 1);
+    expect(array).toEqual([]);
+    expect(global.localStorage.setItem).toBeCalledWith('tasks', JSON.stringify(array));
+  });
+});
